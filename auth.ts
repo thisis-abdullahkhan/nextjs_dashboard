45aab70f73ee34bd726ea5b1111cadad89143e5a
Abdullah Khan <194573922+thisis-abdullahkhan@users.noreply.github.com>
2026-01-07 12:25:26 +0500

    Fixed deployment errors

--- auth.ts
+++ auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
 import Credentials from 'next-auth/providers/credentials';
-import { z } from 'zod/v4';
+import { z } from 'zod';
 import type { User } from './app/lib/definitions';
 import bcrypt from 'bcrypt';
 import postgres from 'postgres';
